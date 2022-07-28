c73fa32ea8836ecaf35f39f8a07093f0458ec9cf
Franck LECUVIER <franck.l@smice.com>
2022-07-28 16:19:41 +0200

    Removed unused DB schema

--- docker/sql/create-additional-users.sql
+++ docker/sql/create-additional-users.sql
@@ -6,8 +6,3 @@ GRANT smice TO smice_database;
 
 GRANT ALL PRIVILEGES ON DATABASE smice TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE smice TO smice_database;
-
-CREATE SCHEMA legacy;
-GRANT ALL PRIVILEGES ON SCHEMA legacy TO smice;
-GRANT ALL PRIVILEGES ON SCHEMA legacy TO postgres;
-GRANT ALL PRIVILEGES ON SCHEMA legacy TO smice_database;
