6046415f86efdd1e9768692dc5e5aba8b4d8e044
Franck LECUVIER <franck.l@smice.com>
2022-07-28 11:47:52 +0200

    Added JWT

---
+++ app/Http/Auth/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Auth\Controllers\AuthController;
+
+//Route::controller(AuthController::class)->group(function () {
+//    Route::post('login', 'login');
+//    Route::post('register', 'register');
+//    Route::post('logout', 'logout');
+//    Route::post('refresh', 'refresh');
+//});
